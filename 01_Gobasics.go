62b63924f18613ad756a80eee3a30714b297304b
Prabhat Singh <singhgprabhat1980@gmail.com>
2025-01-13 15:32:59 +0530

    switch case in go

--- 01_Gobasics.go
+++ 01_Gobasics.go
@@ -4,6 +4,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
 func pow(x, n, lim float64) float64 {
@@ -11,6 +13,7 @@ func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
 	} else {
+		//variable inside the statement are accessable here as well
 		fmt.Printf("%g >= %g\n ", v, lim)
 	}
 	return lim
@@ -40,5 +43,43 @@ func main() {
 	//important point about if and else statements....
 	pow(3, 2, 10)
 	pow(3, 4, 20)
+
+	//lets learn switch case...
+	//break is by default applicable on each case ..
+	//not only number can be used , other thing also can be used...
+
+	// switch val := rand.Intn(3) + 1; val {
+	// case 1:
+	// 	fmt.Print("prabhat")
+
+	// case 2:
+	// 	fmt.Print("Singh")
+	// case 3:
+	// 	fmt.Print("Uber")
+	// }
+
+	//syntex -2
+	val := rand.Intn(3) + 1
+	switch val {
+	case 1:
+		fmt.Print("prabhat")
+
+	case 2:
+		fmt.Print("Singh")
+	case 3:
+		fmt.Print("Uber")
+	}
+
+	//switch statements without condition
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Print("cur time is less than 12")
+	case t.Hour() < 15:
+		fmt.Print("cur time is between 12 and 15")
+	case t.Hour() < 24:
+		fmt.Print("day is about to finish , wrap your task!")
+	}
 
 }
